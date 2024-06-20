58e35540207d378faa662fca1aeec5b6d7ec38db
Rmunene123 <rosewanmun@gmail.com>
2024-06-21 00:09:07 +0300

    added arrays

--- index.js
+++ index.js
@@ -1,1 +1,65 @@
-// Write your solution here!
+let cats =["Milo", "Otis", "Garfield"]
+//console.log(cats)
+
+function destructivelyAppendCat() 
+{
+    cats.push("Ralph")
+}
+function destructivelyPrependCat(){
+    cats.length =0;
+
+
+    cats.push("Bob", "Milo", "Otis", "Garfield")
+}
+
+function destructivelyRemoveLastCat(){
+    cats.length =0;
+
+
+    cats.push("Milo", "Otis")
+}
+
+
+function destructivelyRemoveFirstCat(){
+    cats.length =0;
+
+
+    cats.push("Otis", "Garfield")
+}
+
+
+function appendCat(){
+    cats.length =0;
+
+
+    cats.push("Milo", "Otis", "Garfield")
+    return [...cats,"Broom"]
+}
+
+
+function prependCat(){
+    cats.length =0;
+
+
+    cats.push("Milo", "Otis", "Garfield")
+    return ["Arnold" , ...cats]
+}
+
+
+function removeLastCat(){
+    cats.length =0;
+
+
+    cats.push("Milo", "Otis", "Garfield")
+    return ["Milo", "Otis"]
+}
+
+
+function removeFirstCat(){
+    cats.length =0;
+
+
+    cats.push("Milo", "Otis", "Garfield")
+    return ["Otis", "Garfield"]
+}
+
